b38e62ed8d8778a968f5e12d9e8684284bcd0934
NikitaVr <nvarabei@gmail.com>
2020-05-06 01:47:29 -0400

    connect with ingredients list

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,7 @@ module.exports = {
         path: `${__dirname}/content`,
       },
     },
+    "gatsby-transformer-remark",
     `gatsby-plugin-netlify-cms`,
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,29 @@
 import React from "react"
+import { graphql } from "gatsby"
 
-export default () => <div>Hello world!</div>
+export const query = graphql`
+  query {
+    allFile(
+      filter: { sourceInstanceName: { eq: "content" }, name: { eq: "home" } }
+    ) {
+      edges {
+        node {
+          childMarkdownRemark {
+            frontmatter {
+              title
+              intro
+              image
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+const IndexPage = props => {
+  const data = props.data.allFile.edges[0].node.childMarkdownRemark.frontmatter
+  return <div>data.title</div>
+}
+
+export default IndexPage
