052772d3431c20e49b94cfdfc754a29e63abf361
NikitaVr <nvarabei@gmail.com>
2020-05-06 01:22:52 -0400

    add starter code

---
+++ gatsby-config.js
@@ -0,0 +1,25 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/static/assets`,
+        name: "images",
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/content`,
+      },
+    },
+    `gatsby-plugin-netlify-cms`,
+  ],
+}
